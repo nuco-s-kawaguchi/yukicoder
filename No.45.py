13059ea5dc9dedc5d7b1dc49281ee02f7bf4049a
nuco-s-kawaguchi <shusei_kawaguchi@nuco.co.jp>
2026-04-20 14:39:29 +0900

    リファクタ 変数名を変更

--- No.45.py
+++ No.45.py
@@ -1,20 +1,18 @@
 def main():
-    number_of_plates = int(input())
+    input()
     deliciousness_values = list(map(int, input().split()))
 
-    max_up_to_previous = 0
-    max_up_to_previous_previous = 0
+    take_previous = 0
+    skip_previous = 0
 
     for deliciousness in deliciousness_values:
-        max_up_to_previous_previous, max_up_to_previous = (
-            max_up_to_previous,
-            max(
-                max_up_to_previous,
-                max_up_to_previous_previous + deliciousness
-            )
-        )
+        new_take = skip_previous + deliciousness
+        new_skip = max(skip_previous, take_previous)
 
-    print(max_up_to_previous)
+        take_previous = new_take
+        skip_previous = new_skip
+
+    print(max(take_previous, skip_previous))
 
 
 if __name__ == "__main__":
