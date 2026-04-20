e4f54d8de93ace237cff5477bde95e4d97f8edb8
nuco-s-kawaguchi <shusei_kawaguchi@nuco.co.jp>
2026-04-20 12:47:52 +0900

    リファクタ1

--- No.11.py
+++ No.11.py
@@ -1,4 +1,4 @@
-def count_matching_cards(W, H, hand_cards):
+def count_matching_cards(total_mark_types, total_number_types, hand_cards):
     hand_marks = set()
     hand_numbers = set()
 
@@ -6,22 +6,31 @@ def count_matching_cards(W, H, hand_cards):
         hand_marks.add(mark)
         hand_numbers.add(number)
 
-    mark_match = len(hand_marks) * H
-    number_match = len(hand_numbers) * W
+    mark_match = len(hand_marks) * total_number_types
+    number_match = len(hand_numbers) * total_mark_types
     double_count = len(hand_marks) * len(hand_numbers)
 
     return mark_match + number_match - double_count - len(hand_cards)
 
 
+def read_hand_cards(hand_size):
+    return [tuple(map(int, input().split())) for _ in range(hand_size)]
+
+
 def main():
-    W = int(input())
-    H = int(input())
-    N = int(input())
+    total_mark_types = int(input())
+    total_number_types = int(input())
+    hand_size = int(input())
 
-    hand_cards = [tuple(map(int, input().split())) for _ in range(N)]
+    hand_cards = read_hand_cards(hand_size)
 
-    result = count_matching_cards(W, H, hand_cards)
-    print(result)
+    matching_card_count = count_matching_cards(
+        total_mark_types,
+        total_number_types,
+        hand_cards
+    )
+
+    print(matching_card_count)
 
 
 if __name__ == "__main__":
